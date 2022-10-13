de19511bf5af2269eba587b97a6331eec5e4bfb9
sbfffnico <nicoedrich91@gmail.com>
2022-10-13 01:48:55 -0500

    add nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import './Navbar.css';
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 100) {
+        handleShow(true);
+      } 
+      else {
+        handleShow(false);
+      }
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    });
+  }, [show]);
+
   return (
-    <div>Navbar</div>
+    <div className={`nav ${show && "nav_background"}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158" alt="Netflix Logo" className="nav_logo" />
+      <img src="https://i.imgur.com/WM6zTNc.png" alt="Profile" className="profile_pic" />
+    </div>
   )
 }
 
