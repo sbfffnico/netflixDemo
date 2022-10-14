9715d6f0069869fa353e693ac1cc48b2f5b37eca
sbfffnico <nicoedrich91@gmail.com>
2022-10-14 07:51:25 -0500

    add youtube trailers

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
+import movieTrailer from 'movie-trailer';
 import React, {useState, useEffect} from 'react';
+import YouTube from 'react-youtube';
 import axios from './axios';
 import "./Row.css";
 
@@ -6,6 +8,8 @@ const baseImageURL = "https://image.tmdb.org/t/p/original";
 
 function Row({title, fetchURL, isLargeRow}) {
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState('');
+  const [isPlayed, setIsPlayed] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,9 +21,36 @@ function Row({title, fetchURL, isLargeRow}) {
     fetchData();
   }, [fetchURL]);
 
+  const enablePlayed = () => {
+    setIsPlayed(!isPlayed);
+  }
+
+  function playMovieTrailer (movie) {
+    enablePlayed();
+    if (trailerURL) {
+      setTrailerURL('');
+    }
+    else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+      .then(function (url) {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerURL(urlParams.get('v'));
+      }).catch(function (error) {console.log("Movie trailer error: " + error + ", URL: " + (trailerURL || movie?.title || movie?.name || movie?.original_name || ""))});
+    }
+  }
+
   //console.table(movies);
   //console.log(movies);
   //console.log(movies.map(movie => movie.poster_path));
+
+  const options = {
+    height: "420rem",
+    width: window.innerWidth,
+    playerVars: {
+      allow: "autoplay",
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className='row'>
@@ -28,13 +59,29 @@ function Row({title, fetchURL, isLargeRow}) {
         {movies.map(movie => (
           <img 
               key={movie.id} 
-              className={`row_poster${isLargeRow ? " row_posterLarge" : ""}`}
+              onClick={() => {
+                playMovieTrailer(movie);
+                //enablePlayed();
+                }}
+              className={`row_poster ${isLargeRow ? " row_posterLarge" : ""} ${isPlayed ? " row_poster_played" : ""}`}
               src={`${baseImageURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-              alt={movie.title}
-          >
-          </img>
+              alt={movie?.title || movie?.name || movie?.original_name}
+          />
         ))}
+        {trailerURL && 
+        <YouTube videoId={trailerURL} opts={options} 
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgb(1, 1, 1)",
+          zIndex: "1",
+      }}/>}
       </div>
+        
     </div>
   )
 }
